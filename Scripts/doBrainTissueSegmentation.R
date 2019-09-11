b7785bbbaecd28a0ebff4ff4a489a44808b2eb65
Nick Tustison <ntustison@gmail.com>
2019-09-11 10:08:51 -0700

    ENH: Account for probabilistic mask.

--- Scripts/doBrainTissueSegmentation.R
+++ Scripts/doBrainTissueSegmentation.R
@@ -59,6 +59,8 @@ cat( "Reading ", inputFileName )
 startTime <- Sys.time()
 image <- antsImageRead( inputFileName, dimension = 3 )
 mask <- antsImageRead( inputMaskFileName, dimension = 3 )
+mask[mask >= 0.5] <- 1.0
+mask[mask < 0.5] <- 0.0
 endTime <- Sys.time()
 elapsedTime <- endTime - startTime
 cat( "  (elapsed time:", elapsedTime, "seconds)\n" )
